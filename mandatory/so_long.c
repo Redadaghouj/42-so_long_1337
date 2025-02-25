1c958812d8b36ed9f17a5f574b32c6aa85133f5a
Reda daghouj <mohamedredadaghouj@gmail.com>
2025-02-25 14:53:20 +0000

    so_long

--- mandatory/so_long.c
+++ mandatory/so_long.c
@@ -6,13 +6,13 @@
 /*   By: mdaghouj <mdaghouj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 18:44:52 by mdaghouj          #+#    #+#             */
-/*   Updated: 2025/02/21 13:27:33 by mdaghouj         ###   ########.fr       */
+/*   Updated: 2025/02/25 14:50:02 by mdaghouj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-mlx_image_t	*image;
+mlx_image_t	*img;
 
 void	ft_mlx_error(mlx_t *mlx)
 {
@@ -27,45 +27,55 @@ void	ft_hook(void *param)
 	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(param);
 	else if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
-		image->instances[0].x += 5;
+		img->instances[0].x += 5;
 	else if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
-		image->instances[0].x -= 5;
+		img->instances[0].x -= 5;
 	else if (mlx_is_key_down(mlx, MLX_KEY_UP))
-		image->instances[0].y -= 5;
+		img->instances[0].y -= 5;
 	else if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
-		image->instances[0].y += 5;
+		img->instances[0].y += 5;
 }
 
-void	ft_draw_square(mlx_image_t *image)
-{
-	int	x = 200;
-	int	y = 200;
-	while (y < HEIGHT - 200)
-	{
-		x = 200;
-		while (x < WIDTH - 200)
-		{
-			mlx_put_pixel(image, x, y, 0xFFFFFF);
-			x++;
-		}
-		y++;
-	}
-}
+// void	ft_draw_square(mlx_image_t *img)
+// {
+// 	int	x = 200;
+// 	int	y = 200;
+// 	while (y < HEIGHT - 200)
+// 	{
+// 		x = 200;
+// 		while (x < WIDTH - 200)
+// 		{
+// 			mlx_put_pixel(img, x, y, 0xFFFFFF);
+// 			x++;
+// 		}
+// 		y++;
+// 	}
+// }
 
 int	main(void)
 {
-	mlx_t		*mlx;
+	mlx_t			*mlx;
+	mlx_texture_t	*texture;
 
-	mlx = mlx_init(WIDTH, HEIGHT, "Game", false);
+	mlx = mlx_init(WIDTH, HEIGHT, "AOT", false);
 	if (!mlx)
 		ft_mlx_error(mlx);
-	image = mlx_new_image(mlx, WIDTH, HEIGHT);
-	if (!image)
+	texture = mlx_load_png("mandatory/images/stand_eren.png");
+	if (!texture)
 		ft_mlx_error(mlx);
-	ft_draw_square(image);
+	img = mlx_texture_to_image(mlx, texture);
+	// img = mlx_new_image(mlx, WIDTH, HEIGHT);
+	if (!img)
+		ft_mlx_error(mlx);
+	// ft_draw_square(img);
+	if (mlx_image_to_window(mlx, img, 0, 0) == -1)
+	{
+		mlx_close_window(mlx);
+		ft_mlx_error(mlx);
+	}
 	mlx_loop_hook(mlx, ft_hook, mlx);
-	mlx_image_to_window(mlx, image, 0, 0);
 	mlx_loop(mlx);
+	mlx_delete_image(mlx, img);
 	mlx_terminate(mlx);
 	return (0);
 }
